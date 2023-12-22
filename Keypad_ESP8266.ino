9c5ca259577cfe88c3bd2954d595d42b1eeaa575
Annmon Raji James <annmonjameschiramel@gmail.com>
2023-12-22 16:56:12 +0530

    Defined row and column pins for keypad.

--- Keypad_ESP8266.ino
+++ Keypad_ESP8266.ino
@@ -9,6 +9,11 @@ char keys[ROWS][COLS] = {
   {'*','0','#'}
 };
 
+// Connect keypad ROW0, ROW1, ROW2, and ROW3 to these pins.
+byte rowPins[ROWS] = {D1, D2, D3, D4}; 
+// Connect keypad COL0, COL1, COL2, and COL3 to these pins.
+byte colPins[COLS] = {D5, D6, D7, D8};
+
 void setup() {
   Serial.begin(9600);
 }
