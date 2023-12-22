e88c15073bb010bd54a09fae8b932b9ef26f8011
Annmon Raji James <annmonjameschiramel@gmail.com>
2023-12-22 16:54:23 +0530

    The keypad used is 3*3

--- Keypad_ESP8266.ino
+++ Keypad_ESP8266.ino
@@ -3,10 +3,10 @@
 const byte ROWS = 4; // Four rows
 const byte COLS = 4; // Four columns
 char keys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
 };
 
 void setup() {
